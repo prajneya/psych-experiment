baa73d93cffd7373fb4d3aef76aa1ff52ebac96e
Parardha Kumar <parardhakumar@gmail.com>
2021-11-18 13:14:17 +0530

    restructure files

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -25,4 +25,4 @@ app.use("/api/experiment", experiment);
 
 const port = process.env.PORT || 5000;
 
-app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
