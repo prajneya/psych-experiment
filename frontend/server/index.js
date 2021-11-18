7df6e63a6a8759810d7a73541324da0a05b41c66
Parardha Kumar <parardhakumar@gmail.com>
2021-11-18 14:22:13 +0530

    update server/index.js

--- frontend/server/index.js
+++ frontend/server/index.js
@@ -7,11 +7,11 @@ const connectDB = require('./config/db')
 
 // routes
 const experiment = require('./routes/api/experiment');
-app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.use(express.static(path.join(__dirname, '../build')))
 
 // AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/../frontend/build/index.html'))
+  res.sendFile(path.join(__dirname + '/../build/index.html'))
 })
 // Connect Database
 connectDB();
